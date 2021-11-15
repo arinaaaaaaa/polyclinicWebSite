eacd4b5dde102017e0352f6e653a87a207f2c71f
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2021-11-15 09:27:27 +0300

    model of polyclinic was complite

--- backend/polyclinic/models.py
+++ backend/polyclinic/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Polyclinic (models.Model):
+    type = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    metro = models.CharField(max_length=50)
 
     @classmethod
-    def create(cls, address):
-        polyclinic = cls(address = address)
+    def create(cls, type, name, address, phone, metro):
+        polyclinic = cls(type = type, name = name, address = address, phone = phone, metro = metro)
         return polyclinic
